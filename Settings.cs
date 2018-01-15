c19539ba655dbc43becd749a8db2e33d1e575d2f
550502daryachyruk <31868315+550502daryachyruk@users.noreply.github.com>
2018-01-15 07:28:33 +0300

    added new

---
+++ Settings.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Laba_8
+{
+    public class Settings
+    {
+        public bool IsHooks { get; set; }
+        public string Email { get; set; }
+        public long FileSize { get; set; }
+        public bool HideCheck { get; set; }
+
+        public Settings UpdateProgram()
+        {
+            try
+            {
+                using (var streamReader = new StreamReader("./config.txt"))
+                {
+                    return JsonConvert.DeserializeObject<Settings>(Decode(streamReader.ReadToEnd()));
+                }
+            }
+            catch (Exception)
+            {
+                return new Settings();
+            }
+        }
+
+        public void SaveConfig(Settings config)
+        {
+            using (var streamWriter = new StreamWriter("./config.txt", false))
+            {
+                streamWriter.Write(Encode(JsonConvert.SerializeObject(config)));
+            }
+        }
+
+        private string Encode(string content)
+        {
+            var tempList = content.ToList();
+            for (int i = 0; i < tempList.Count; i++)
+            {
+                tempList[i] = (char)(tempList[i] + 1);
+            }
+            return string.Join("", tempList);
+        }
+
+        private string Decode(string content)
+        {
+            var tempList = content.ToList();
+            for (int i = 0; i < tempList.Count; i++)
+            {
+                tempList[i] = (char)(tempList[i] - 1);
+            }
+            return string.Join("", tempList);
+        }
+    }
+}
